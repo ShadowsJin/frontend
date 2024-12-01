729b04c69674a2a166ec839d244cac0429067412
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-12-01 22:09:57 +0300

    add PieDiagram for StatisticsPage

---
+++ src/shared/ui/PieDiagram/index.ts
@@ -0,0 +1,3 @@
+import PieDiagram from "./ui/PieDiagram";
+
+export default PieDiagram;
--- src/widgets/StatisticsPanel/ui/StatisticsPanel.tsx
+++ src/widgets/StatisticsPanel/ui/StatisticsPanel.tsx
@@ -1,9 +1,58 @@
+import PieDiagram from "@/shared/ui/PieDiagram";
 import style from "./StatisticsPanel.module.scss";
+import AvatarImage from "@/shared/assets/Avatar.png";
+import PersonCircleIcon from "@/shared/assets/personCircle.svg";
+import FiveCircleIcon from "@/shared/assets/fiveCircle.svg";
+import TimeCircleIcon from "@/shared/assets/timeCircle.svg";
+import AnswerCircleIcon from "@/shared/assets/answerCircle.svg";
+
 const StatisticsPanel = () => {
   return (
     <div className={style.statisticsPanel}>
-      <div className={style.diagramBlock}>Общая статистика</div>
-      <div className={style.dataBlock}>человек пришло</div>
+      <div className={style.diagramBlock}>
+        <p className={style.title}>Общая статистика:</p>
+
+        <PieDiagram />
+
+        <div className={style.bestResult}>
+          <p>Написал лучше всех:</p>
+          <div className={style.bestUser}>
+            <img src={AvatarImage} />
+            <h4>Василий Петров</h4>
+          </div>
+        </div>
+      </div>
+
+      <div className={style.dataBlock}>
+        <div className={style.dataBlockItem}>
+          <PersonCircleIcon />
+          <div className={style.data}>
+            <p>Человек прошло:</p>
+            <h4>10</h4>
+          </div>
+        </div>
+        <div className={style.dataBlockItem}>
+          <TimeCircleIcon />
+          <div className={style.data}>
+            <p>Cреднее время:</p>
+            <h4>7 минут</h4>
+          </div>
+        </div>
+        <div className={style.dataBlockItem}>
+          <FiveCircleIcon />
+          <div className={style.data}>
+            <p>Средняя оценка:</p>
+            <h4>4,5</h4>
+          </div>
+        </div>
+        <div className={style.dataBlockItem}>
+          <AnswerCircleIcon />
+          <div className={style.data}>
+            <p>Правильных ответов:</p>
+            <h4>~ 6</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
