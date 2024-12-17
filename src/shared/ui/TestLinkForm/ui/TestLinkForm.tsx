ce500188a77250413a94105096c1407dba106aea
lizakobzeva <143506202+lizakobzeva@users.noreply.github.com>
2024-12-17 11:18:27 +0300

    add copy for TestLink

--- src/shared/ui/TestLinkForm/ui/TestLinkForm.tsx
+++ src/shared/ui/TestLinkForm/ui/TestLinkForm.tsx
@@ -1,14 +1,29 @@
+import notification from "@/shared/config/ApiConfig/Notification";
 import style from "./TestLinkForm.module.scss";
 import { APP_URL } from "@/shared/constants/appURL";
+import Button from "../../Button";
 interface TestLinkFormProps {
   link: string;
   closeModal: () => void;
 }
+
 const TestLinkForm = ({ link, closeModal }: TestLinkFormProps) => {
+  const copyTextToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(`${APP_URL}${link}`);
+      notification("Текст успешно скопирован в буфер обмена!", "success");
+    } catch (err) {
+      notification("Ошибка, не получилось скопировать ссылку", "error");
+    }
+  };
   return (
     <div className={style.testLinkForm}>
       Ссылка на прохождение теста:{" "}
-      <h4 onClick={closeModal}>{`${APP_URL}${link}`}</h4>
+      <h4
+        className={style.testLink}
+        onClick={copyTextToClipboard}
+      >{`${APP_URL}${link}`}</h4>
+      <Button onClick={closeModal}>Отмена</Button>
     </div>
   );
 };
